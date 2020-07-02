696a18864e892093efb9497b3ad246482eee617a
Olivia Davis <olivia.davis593@gmail.com>
2020-07-01 22:43:09 -0500

    Big O & array implementation

--- array.js
+++ array.js
@@ -1,29 +1,128 @@
+const memory = require('./memory');
 
-
-//Initializing & pushing 
+//building our array 
 class Array {
-    //Array starts with length & pointer of 0 blocks of memory 
     constructor() {
+        //we start with pretty much nothing 
         this.length = 0; 
-        this.ptr = memory.allocate(this.length)
+        this.ptr = memory.allocate(this.length);
     }
 
-    push(value) {
-        //resize the array so there's space for the new item 
-        this._resize(this.length + 1);
-        memory.set(this.ptr + this.length, value);
-        this.length++; 
-    }
-
-    _resize(size) {
-        const oldPtr = this.ptr; 
-        this.ptr = memory.allocate(size)
-        if (this.ptr === null) {
-            throw new Error('Out of memory')
+    //to push an element to end of an array 
+    // we need to increase the amount of memory slots to invent space for new item
+    //then we need to set that value of the memory slot(s) to contain the new value 
+    /*Big O: 
+    1. push method resizes the array 
+    2. it then increases the length & sets a memory address (both O(1) operations) === push is O(n)
+    3. how to make it better
+        - rather than resizing => everytime you push some data - you could allocate more space than needed
+        - that way you need to resize far less often:
+        push(value) {
+            if(this.length >= this._capacity) {
+                this._resize((this.length + 1) * Array.SIZE_RATIO);
+            }
+            memory.set(this.ptr + this.length, value);
+            this.length++
         }
-        memory.copy(this.ptr, oldPtr, this.length);
-        memory.free(oldPtr);
+        - now you length & capacity => how many items you can hold w.o needing to resize 
+        - if the length is > than the capacity => you'd resize according to SIZE_RATIO
+        - i.e each time you go over capacity => you triple size of memory allocated 
+        - Big O best case: O(1) => you won't need to resize
+        - Big O worst case: O(n) => you'd still need to resize so it remains O(n)
+        - the tradeoff: 
+        1. you're wasting some memory when the capacity is greater than the length 
+        2. isn't really a big deal = pushing to array is incredibly common & worthwhile optimization to give O(1) performance
+     */
+    push(value) {
+        this._resize(this.length + 1);
+        //we're making sure that ptr & length are no longer 0 but are set 1 === value 
+        //we're making sure that we set the value as well 
+        memory.set(this.ptr + this.length, value);
+        //to get the address of the nth item === add n to the memory adress of the start of the data
+        this.length++
     }
+
+    //its unlikely that you'd be able to ask for extra slots directly at the end of currently allocated space
+    /*so you have to:
+    1. allocate a new larger chunk of memory 
+    2. copy any existing values from the old chunk to the new chunk
+    3. free the old chunk 
+    */
+   /*Big O: 
+   1. you have to copy each item of data (oldPtr) to a new box each time you resize the
+   2. so n copies: your resize operation has worst, best & average case of linear (O(n))
+    */
+   _resize(size) {
+       const oldPtr = this.ptr; 
+       this.ptr = memory.allocate(size);
+       if (this.ptr === null) {
+           throw new Error('Out of memory')
+       }
+       memory.copy(this.ptr, oldPtr, this.length);
+       memory.free(oldPtr)
+   }
+
+   //retrieving is straightforwards 
+   //just like pushing you'd find the correct memory address by simpling adding the index to the start position
+   /*Big O: 
+   1. Both operations are O(1)
+   2. retrieving values from any point in an array also has best, worst & average performance of O(1)
+    */
+   get(index) {
+       if (index < 0 || index >= this.length) {
+           throw new Error('Index error')
+       }
+       //adds an index offset 
+       // gets the value stored at a memory addres
+       return memory.get(this.ptr +  index);
+   }
+
+   //popping a value from the end of an array 
+   //rather than resize the array => you'd just leave extra space which will be filed at the next push 
+   //Big O: we're just removing something but still leaving the space empty rather than deleting it === O(1)
+   pop() {
+       if (this.length === 0) {
+           throw new Error('Index error')
+       }
+       const value = memory.get(this.ptr + this.length - 1);
+       this.length--; 
+       return value; 
+   }
+
+   //inserting a value 
+   //if we want to insert a value at any point (not just middle) 
+   //we'd shift all of the values after the new value back 1 position
+   //then put the new value in its correct place 
+   /*Big O:
+   1. Best case: you're inserting the value at the back of the array (same as pushing) === O(1)
+   2. Worst case: inserting the value at the start of the array === O(n)
+    - this requires every value to be shifted 1 memory address later
+   3. Average case: inserting a value into the middle of array
+    - you'd need to shift half of the values along === n/2
+    - Answer: O(n) => you ignore the constant factor 1/2
+   */
+   insert(index, value) {
+       if (index < 0 || index >= this.length) {
+           throw new Error('Index error')
+       } 
+       if (this.length >= this._capacity) {
+           this._resize((this.length + 1) * Array.SIZE_RATIO);
+       }
+       memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+       memory.set(this.ptr + index, value);
+       this.length++
+   }
+
+   //removing value = similar to inserting values 
+   //difference: you're copying the values backwards to fill space where you removed the value
+   //rather than forwards to make space for a new value 
+   /*Big O:
+   1. Best case: O(1) === same as popping
+   2. Average & Worst case: O(n)
+   
+   */
+
 }
 
-Array.SIZE_RATIO = 3; 
+
+export default Array; 
--- memory.js
+++ memory.js
@@ -1,9 +1,12 @@
+//Simulates a block of memory
 class Memory {
   constructor() {
     this.memory = new Float64Array(1024);
     this.head = 0;
   }
 
+  //reserves a contiguous block of memory slots
+  //you can also reserve empty (null) slots in the process
   allocate(size) {
     if (this.head + size > this.memory.length) {
       return null;
@@ -15,8 +18,10 @@ class Memory {
     return start;
   }
 
+  //free slots reserved using allocate 
   free(ptr) {}
 
+  //copy address in memory slot & can move to one pointer from another pointer
   copy(toIdx, fromIdx, size) {
     if (fromIdx === toIdx) {
       return;
@@ -35,10 +40,14 @@ class Memory {
     }
   }
 
+  //return the value stored at certain memory address 
+  //ptr: points to a specific memory slot 
   get(ptr) {
     return this.memory[ptr];
   }
 
+  //set the values stored at a certain memory address
+  //ex: if we want 'Apple' in slot 0 then we'd set it in address 0? 
   set(ptr, value) {
     this.memory[ptr] = value;
   }
